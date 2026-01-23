90d0c565661b2ea128599512d837b06f765e7918
Irfan aslam <irfanaslamo700@gmail.com>
2026-01-23 17:21:37 +0530

    add some

--- UserPlanDetils/Plan.js
+++ UserPlanDetils/Plan.js
@@ -1,15 +1,33 @@
 const jwt = require('jsonwebtoken')
 const User = require("../models/userSchema");
+const GroundSeatBooking = require("../models/Groundflor");
+const topSeetBooking = require("../models/Topflor")
 
 // add  user  plan detials
 const Adddata = async(req, res) => {
     try {
         const token = req.cookies.token;
+        console.log(req.body)
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const _id = decoded.id;
         const plandata = JSON.parse(req.body.PlaneData);
         const user2 = await User.findById(_id);
         if (user2.Active) return res.status(404).json({ massage: "You  can not book becouse you allready  booked  you cand  changa  nad  extend  youre  subcription" })
+        const startDate = new Date(req.body.startDate);
+        const endDate = new Date(startDate);
+        endDate.setDate(endDate.getDate() + Number(req.body.Day_Remining || 0));
+        const formattedEndDate = endDate.toISOString().split("T")[0];
+        if (req.body.Location && req.body.Seat_Number != "0") {
+            const creted = await topSeetBooking.create({ seatNumber: req.body.Seatnumber, email: user2.email });
+            if (!creted) return res.status(404).json({ massage: "set is  not avalibel" })
+        } else {
+            if (req.body.Seatnumber != "0") {
+                const creted = await GroundSeatBooking.create({ seatNumber: req.body.Seatnumber, email: user2.email });
+                if (!creted) return res.status(404).json({ massage: "set is  not avalibel" })
+
+            }
+
+        }
         await User.findByIdAndUpdate(
             _id, {
                 $set: {
@@ -21,21 +39,22 @@ const Adddata = async(req, res) => {
                     Location: plandata.premium,
                     Active: true,
                     Starting_Date: req.body.startDate,
-                    Day_Remining: 30,
+                    Day_Remining: req.body.Day_Remining,
                     Locker: req.body.locker,
                     Floor: plandata.floors,
-                    Totalamount: req.body.Totalamount
+                    Totalamount: req.body.Totalamount,
+                    Enddate: formattedEndDate
                 }
             }, { new: true }
         );
-        res.status(200).json({ massage: "heloo" })
+
+        res.status(200).json({ massage: "bookend" })
     } catch (error) {
         console.log(error)
+        res.status(500).json({ massage: error })
     }
 
 }
-
-// user data
 const SendData = async(req, res) => {
     try {
         const token = req.cookies.token;
@@ -57,6 +76,12 @@ const Seatupdate = async(req, res) => {
         const _id = decoded.id;
         const userfound = await User.findById(_id);
         userfound.Seat_Number = req.body.seat;
+        if (userfound.Location) {
+            await topSeetBooking.findOneAndUpdate({ email: userfound.email }, { seatNumber: req.body.seat });
+        } else {
+            await GroundSeatBooking.findOneAndUpdate({ email: userfound.email }, { seatNumber: req.body.seat });
+
+        }
         await userfound.save()
         res.status(200).json({ massage: "seat   update conform" })
 
@@ -66,7 +91,31 @@ const Seatupdate = async(req, res) => {
     }
 }
 
-// cancil  subscription just  add  active  false
+const gaetSeat = async(req, res) => {
+    try {
+        const booked = await GroundSeatBooking.find({}, "seatNumber");
+
+        const bookedSeats = booked.map(b => b.seatNumber);
+        res.status(200).json({ data: bookedSeats })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ massage: "somthing  went  wronge" })
+
+    }
+}
+const topflor = async(req, res) => {
+        try {
+            const booked = await topSeetBooking.find({}, "seatNumber");
+
+            const bookedSeats = booked.map(b => b.seatNumber);
+            res.status(200).json({ data: bookedSeats })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ massage: "somthing  went  wronge" })
+
+        }
+    }
+    // cancil  subscription just  add  active  false
 const CancilSubscription = async(req, res) => {
     try {
         const token = req.cookies.token;
@@ -74,6 +123,14 @@ const CancilSubscription = async(req, res) => {
         const _id = decoded.id;
         const userfound = await User.findById(_id);
         userfound.Active = false;
+        console.log("loction ", userfound.Location);
+
+        if (userfound.Location) {
+            await topSeetBooking.deleteOne({ email: userfound.email })
+        } else {
+            await GroundSeatBooking.deleteOne({ email: userfound.email });
+        }
+
         await userfound.save()
         res.status(200).json({ massage: "Subcription cancile" })
     } catch (error) {
@@ -117,4 +174,6 @@ const descrase = async(req, res) => {
 
 }
 
-module.exports = { Adddata, SendData, Seatupdate, CancilSubscription, descrase }
+
+
+module.exports = { Adddata, SendData, Seatupdate, CancilSubscription, descrase, gaetSeat, topflor }
--- router/UserRouting.js
+++ router/UserRouting.js
@@ -1,7 +1,7 @@
 const { signup, login, forgetPassword, Verifyemail, OTPverify, verfyOTP, Logout, isLoging } = require("../controller/UserController");
 const upload = require('../Middlerwere/Multler')
 const Autho = require('../Middlerwere/Autho')
-const { Adddata, SendData, Seatupdate, CancilSubscription, descrase } = require('../UserPlanDetils/Plan')
+const { Adddata, SendData, Seatupdate, CancilSubscription, descrase, gaetSeat, topflor } = require('../UserPlanDetils/Plan')
 const router = require("express").Router();
 
 router.post('/signup', signup);
@@ -18,5 +18,7 @@ router.post('/Adduserplanedata', Autho, upload.single('UserImg'), Adddata)
 router.post('/SendData', Autho, SendData);
 router.put("/Seatupdate", Autho, Seatupdate);
 router.put("/CancilSubscription", Autho, CancilSubscription);
-router.get('/descrase', descrase)
+router.get('/descrase', Autho, descrase)
+router.get('/gaetSeat', gaetSeat)
+router.get('/topflor', topflor)
 module.exports = router;
