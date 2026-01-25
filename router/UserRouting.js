07a514a78851ade89590ba1c2814dc4977523180
Irfan aslam <irfanaslamo700@gmail.com>
2026-01-25 23:19:23 +0530

    done everything

--- router/UserRouting.js
+++ router/UserRouting.js
@@ -1,7 +1,8 @@
 const { signup, login, forgetPassword, Verifyemail, OTPverify, verfyOTP, Logout, isLoging } = require("../controller/UserController");
 const upload = require('../Middlerwere/Multler')
 const Autho = require('../Middlerwere/Autho')
-const { Adddata, SendData, Seatupdate, CancilSubscription, descrase, gaetSeat, topflor, cheqActive } = require('../UserPlanDetils/Plan')
+const { Adddata, SendData, Seatupdate, CancilSubscription, descrase, gaetSeat, topflor, cheqActive, complainpush, getComplain, markCompletcomplain } = require('../UserPlanDetils/Plan')
+const { deaskbordData, Deleteuser } = require('../AminAccess/Addmin')
 const router = require("express").Router();
 
 router.post('/signup', signup);
@@ -22,4 +23,11 @@ router.get('/descrase', Autho, descrase)
 router.get('/gaetSeat', Autho, gaetSeat)
 router.get('/topflor', Autho, topflor)
 router.post('/cheqActive', Autho, cheqActive)
+
+// admin 
+router.get('/deaskbordData', deaskbordData)
+router.delete('/delsete/:id', Deleteuser)
+router.post('/Pushcomplain', complainpush)
+router.get('/getComplain', getComplain);
+router.put('/markcomplete/:id', markCompletcomplain)
 module.exports = router;
