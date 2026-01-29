2300f8a097bb75953d14cc3394b6a4852753e43f
Irfan aslam <irfanaslamo700@gmail.com>
2026-01-29 22:56:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://library-three-mocha.vercel.app",
+    origin:[ "https://library-three-mocha.vercel.app","http://localhost:5173"],
     credentials: true
 }));
 app.use(cookieParser());
